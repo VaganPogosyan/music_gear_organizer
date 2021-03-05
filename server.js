6543897b2a0413fa7c49dc77aa343a5ff9fb8832
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-03-04 23:19:21 -0500

    update code for heroku

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const APP = express();
-const PORT = 3000;
+// const PORT = 3000;
+const db = mongoose.connection;
+
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000;
+
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + `gear`;
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open', () => { });
+
+
+
+// mongoose conection
+// mongoose.connect('mongodb://localhost:27017/gear', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongo');
+// });
+
 
 // controller logic
 const gearController = require('./controllers/gear.js');
@@ -13,11 +40,7 @@ APP.use(express.urlencoded({ extended: true }));
 APP.use(methodOverride('_method'));
 APP.use(express.static('public'));
 
-// mongoose conection
-mongoose.connect('mongodb://localhost:27017/gear', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => {
-    console.log('connected to mongo');
-});
+
 
 
 // listener
