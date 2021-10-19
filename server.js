cf372d10fc879a7188f53e287c6891086f8871be
Vagan Pogosyan <vagan2502@gmail.com>
2021-10-19 13:31:08 -0400

    server.js debugging

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const APP = express();
 // const PORT = 3000;
 const db = mongoose.connection;
@@ -9,37 +9,40 @@ const db = mongoose.connection;
 const PORT = process.env.PORT || 3000;
 
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + `gear`;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/" + `gear`;
 
 // Connect to Mongo
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // open the connection to mongo
-db.on('open', () => { });
+db.on("open", () => {});
 
 // middleware to help with the form submission
 APP.use(express.urlencoded({ extended: true }));
-APP.use(methodOverride('_method'));
-APP.use(express.static('public'));
+APP.use(methodOverride("_method"));
+APP.use(express.static("public"));
 
 // mongoose conection
-// mongoose.connect('mongodb://localhost:27017/gear', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connection.once('open', () => {
-//     console.log('connected to mongo');
-// });
-
+mongoose.connect("mongodb://localhost:27017/gear", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
 // controller logic
-const gearController = require('./controllers/gear.js');
-APP.use('/gear', gearController);
-
-
-
+const gearController = require("./controllers/gear.js");
+APP.use("/gear", gearController);
 
 // APP.get('/', (req, res) => {
 //     res.send('Hello world!');
@@ -47,5 +50,5 @@ APP.use('/gear', gearController);
 
 // listener
 APP.listen(PORT, () => {
-    console.log('Lisening on port: ' + PORT);
+  console.log("Lisening on port: " + PORT);
 });
